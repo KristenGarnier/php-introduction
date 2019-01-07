362fcdf47f12f505c8990dccf6495493e6ee3684
Kristen Garnier <kristengarnier@MacBook-Pro-de-Kristen.local>
2019-01-07 15:54:16 +0100

    Complete exercice 4

---
+++ public/country.php
@@ -0,0 +1,40 @@
+<?php
+ // ~/php/tp1/public/cities.php
+ // include model
+ include __DIR__ . '/../model/cities.php';
+ if(!key_exists('name', $_GET)) {
+    page_not_found();
+ }
+ $country = $_GET['name'];
+
+ if(!do_country_exists($country, $cities)) {
+    page_not_found();
+ }
+
+ $citiesFromCountry = []; // Will be used to contain all our countries
+ foreach($cities as $city) {
+     if( $city['country'] === $country ) { // Check if country does not already exists in array
+         array_push($citiesFromCountry, $city);
+     }
+ }
+
+ // include view
+ include __DIR__ . '/../view/country.php';
+
+ function page_not_found()
+{
+    header("HTTP/1.0 404 Not Found"); //On définit la page comme étant une page 404 au sein de l’entête
+    include __DIR__ . '/../view/404.html'; // On ajoute la vue de la page 404
+    die(); // arrête l’exécution du script
+}
+
+function do_country_exists($name, $cities) {
+    $result = false;
+    foreach($cities as $city) {
+        if( $city['country'] === $name ) { // Check if country does not already exists in array
+            $result = true;
+        }
+    }
+
+    return $result;
+}
